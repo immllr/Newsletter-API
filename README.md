845a5f5d2eb3a8965ca8b4c1595f03ba1e0a8bd8
dmueller <#Dmsk1609tn>
2019-03-22 14:25:58 +0100

    [TASK] add basic files and functions

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ finaly open in your browser
 ```
 http://localhost:3030/headline/desktop/Arial/15px/222222/35px/ffffff/50/Hallo%20ich%20bin%20ein%20Text
 ```
+
+
+
+
+-moz-osx-font-smoothing: grayscale; -webkit-font-smoothing: antialiased; text-rendering: optimizeLegibility;
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var fs = require('fs');
+var webshot = require('webshot');
 var app = express();
+
 
 /**
  *
@@ -24,31 +26,50 @@ app.listen(3030, () => {
 
 app.get("/headline/:reqDevice/:reqFontFamily/:reqFontSize/:reqFontColor/:reqLineHeight/:reqBackground/:reqPadding/:reqText", (req, res, next) => {
 
-    /*res.json([{ 'Device': req.params.reqDevice,
-                'font-family': req.params.reqFontFamily,
-                'font-size': req.params.reqFontSize,
-                'color': req.params.reqFontColor,
-                'line-height': req.params.reqLineHeight,
-                'background': req.params.reqBackground,
-                'padding': req.params.reqPadding,
-                'text': req.params.reqText
-    }]);*/
-    let rawID = req.params.reqDevice + req.params.reqFontFamily + req.params.reqFontSize + req.params.reqFontColor + req.params.reqLineHeight + req.params.reqBackground + req.params.reqPadding + req.params.reqText;
+    var rawID = req.params.reqDevice + req.params.reqFontFamily + req.params.reqFontSize + req.params.reqFontColor + req.params.reqLineHeight + req.params.reqBackground + req.params.reqPadding + req.params.reqText;
+    rawID = rawID.toString().hashCode();
+    console.log("[LOG] image ID " + rawID + " requested");
 
 
-
-    fs.exists('cache/' + rawID.toString().hashCode() + '.png', function(exists) {
+    fs.exists('cache/' + rawID + '.png', function(exists) {
         if (exists) {
-            fs.readFile('cache/' + rawID.toString().hashCode() + '.png', function (err,data) {
+            console.log("[LOG] image ID " + rawID + " is cached");
+            fs.readFile('cache/' + rawID + '.png', function (err,data) {
                 if (err) {
                     return console.log(err);
                 }
                 res.writeHead(200, {"Content-Type": "image/png"});
                 res.write(data);
+                console.log("[LOG] image ID " + rawID + "delivered");
             });
 
         } else {
-            console.log('NOT OK');
+            console.log("[LOG] image ID " + rawID + " not cached");
+
+            var options = {
+                windowSize: { width: 600
+                    , height: 'auto' },
+                shotSize: {
+                    width: 600
+                },
+                siteType:'html',
+                quality: 100
+            };
+
+            var htmlCode = "<html><head><link href=\"https://fonts.googleapis.com/css?family=Indie+Flower\" rel=\"stylesheet\"><style>* { box-sizing: border-box } body { width: 600px; margin: 0; background-color: "+req.params.reqBackground+" } h1 { -moz-osx-font-smoothing: grayscale; -webkit-font-smoothing: antialiased; text-rendering: optimizeLegibility; font-family:"+req.params.reqFontFamily+"; font-size: "+req.params.reqFontSize+"; color: "+req.params.reqFontColor+" }</style></head><body><h1>"+req.params.reqText+"</h1></body></html>"
+
+            webshot(htmlCode, 'cache/' + rawID + '.png', options, function(err) {
+                fs.readFile('cache/' + rawID + '.png', function (err,data) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    res.writeHead(200, {"Content-Type": "image/png"});
+                    res.write(data);
+                    console.log("[LOG] image ID " + rawID + "delivered");
+                });
+            });
+
+            console.log("[LOG] image ID " + rawID + " generated and delivered");
         }
     });
 
